a77c850061d9aa99d7444d83a301bd081605850a
Armin Schrenk <armin.schrenk@skymatic.de>
2021-10-05 13:43:14 +0200

    Fixing sem ver check by using literal regex in js and officail semver regex (old regex failed matching 1.6.0-rc1)
    ​

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ try {
   const version = core.getInput('version');
   console.log(`Checking version: ${version}!`);
   
-  var re = new RegExp("(0|[1-9]\d*)+\.(0|[1-9]\d*)+\.(0|[1-9]\d*)+(-(([a-z-][\da-z-]+|[\da-z-]+[a-z-][\da-z-]*|0|[1-9]\d*)(\.([a-z-][\da-z-]+|[\da-z-]+[a-z-][\da-z-]*|0|[1-9]\d*))*))?(\\+([\da-z-]+(\.[\da-z-]+)*))?$", "i");
+  //regex taken from semver.org, see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+  var re = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
   if (re.exec(version) == null) {
     core.setFailed("Invalid version");
   }
