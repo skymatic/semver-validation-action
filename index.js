f89451a3f716b6aebbd21f7625828e60f21efb5d
Armin Schrenk <armin.schrenk@skymatic.de>
2022-11-23 12:13:25 +0100

    remove unused dependency

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setFailed } from '@actions/core';
 
 try {
   // `version` input defined in action metadata file
-  const version = core.getInput('version');
+  const version = getInput('version');
   console.log(`Checking version: ${version}!`);
 
   //regex taken from semver.org, see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
   var re = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
   if (re.exec(version) == null) {
-    core.setFailed("Invalid version");
+    setFailed("Invalid version");
   }
 
 } catch (error) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
