7942fa45c9cabca1319bad5ec86ced4a8f4ce528
Armin Schrenk <armin.schrenk@skymatic.de>
2022-11-23 12:34:15 +0100

    revert using es modules

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import { getInput, setFailed } from '@actions/core';
+const core = require('@actions/core');
 
 try {
   // `version` input defined in action metadata file
-  const version = getInput('version');
+  const version = core.getInput('version');
   console.log(`Checking version: ${version}!`);
 
   //regex taken from semver.org, see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
   var re = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
   if (re.exec(version) == null) {
-    setFailed("Invalid version");
+    core.setFailed("Invalid version");
   }
 
 } catch (error) {
-  setFailed(error.message);
+  core.setFailed(error.message);
 }
